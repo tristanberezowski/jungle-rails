48ef0721b8e3e956b7606e5d82884444600cd602
Tristan <tristanberezowski@gmail.com>
2019-03-22 02:08:04 +0000

    logged in on account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to "/", notice: 'User created!'
     else
       render :new
