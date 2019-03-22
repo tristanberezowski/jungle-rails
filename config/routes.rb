d97ccbb9e999c207618ad37b684248ee270ebfcc
Tristan <tristanberezowski@gmail.com>
2019-03-22 01:56:23 +0000

    feature implemented properly

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  put '/login' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   resource :cart, only: [:show] do
     post   :add_item
