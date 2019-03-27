bbac9e12b11e94c2f2311252ac1b551ce1746f78
Tristan <tristanberezowski@gmail.com>
2019-03-27 02:00:06 +0000

    tests to make sure the link to show product works correctly

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+    
+    @category.products.create!(
+      name:  "Cool Shirt",
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+    @category.products.create!(
+      name:  "Bad Shirt",
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "They see the shown product" do
+    visit root_path
+    click_on 'Cool Shirt'
+
+    # commented out b/c it's for debugging only
+    # save_and_open_screenshot
+
+    expect(page.has_css?('dd', text: 'Cool Shirt')).to eq true
+    expect(page.has_css?('dd', text: 'Bad Shirt')).to eq false
+
+  end
+end
